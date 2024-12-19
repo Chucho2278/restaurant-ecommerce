76063a7bf28099ca6217b7a7573aa9755d541675
Jesús Cañón <xuxo_22@hotmail.com>
2024-12-18 22:47:24 -0500

    versión 2.4 (package actualizado 1.0)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,28 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import {
+  provideRouter,
+  withComponentInputBinding,
+  Route,
+} from '@angular/router';
+
+const appRoutes: Route[] = [
+  {
+    path: 'users',
+    loadComponent: () =>
+      import('./users/user-list/user-list.component').then(
+        (m) => m.UserListComponent
+      ),
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', redirectTo: '/home' },
+];
 
 const appConfig: ApplicationConfig = {
-  providers: [provideHttpClient()],
+  providers: [
+    provideHttpClient(),
+    provideRouter(appRoutes, withComponentInputBinding()),
+  ],
 };
 
 export default appConfig;
