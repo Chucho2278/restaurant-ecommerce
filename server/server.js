5efdc694260c62b0df86f3ab63c2305c17d5f297
Jesús Cañón <xuxo_22@hotmail.com>
2024-12-18 22:00:31 -0500

    Versión 2.4

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const mongoose = require("./db"); // Esto importará tu configuración de MongoDB
+const path = require("path");
+const mongoose = require("./db"); // Importará tu configuración de MongoDB
 const cors = require("cors");
 require("dotenv").config();
 
@@ -13,9 +14,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Servir archivos estáticos de Angular
+app.use(express.static(path.join(__dirname, "dist/restaurant-ecommerce")));
+
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api", orderRoutes);
+
+// Manejar todas las demás rutas y devolver el archivo index.html de Angular
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/restaurant-ecommerce/index.html"));
+});
 
 app.use((req, res, next) => {
   console.log(`Incoming request: ${req.method} ${req.url}`);
