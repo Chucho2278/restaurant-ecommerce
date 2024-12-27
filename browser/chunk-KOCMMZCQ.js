aafd91cb26e5cae5bd5a7b67e109d32b6faed48a
Jesús Cañón <xuxo_22@hotmail.com>
2024-12-26 20:51:18 -0500

    Updates

---
+++ browser/chunk-KOCMMZCQ.js
@@ -0,0 +1,1 @@
+import{Ua as n,Va as o,_a as i,ca as s,eb as r}from"./chunk-U3ZKRPLZ.js";var a=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=s({type:t,selectors:[["app-user-list"]],standalone:!0,features:[r],decls:2,vars:0,template:function(e,l){e&1&&(n(0,"p"),i(1,"user-list works!"),o())}})};export{a as UserListComponent};
